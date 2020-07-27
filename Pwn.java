353ed93e05568bec85bc2b43b595cebe54c30e7c
Alvaro Muñoz <pwntester@github.com>
2020-07-28 00:17:35 +0200

    first commit

---
+++ Pwn.java
@@ -0,0 +1,11 @@
+public class Pwn {
+    public static String PWN = "";
+    static {
+        System.out.println(".______   ____    __    ____ .__   __.  _______  _______   __  ");
+        System.out.println("|   _  \\  \\   \\  /  \\  /   / |  \\ |  | |   ____||       \\ |  | ");
+        System.out.println("|  |_)  |  \\   \\/    \\/   /  |   \\|  | |  |__   |  .--.  ||  | ");
+        System.out.println("|   ___/    \\            /   |  . `  | |   __|  |  |  |  ||  | ");
+        System.out.println("|  |         \\    /\\    /    |  |\\   | |  |____ |  '--'  ||__| ");
+        System.out.println("| _|          \\__/  \\__/     |__| \\__| |_______||_______/ (__) ");
+    }
+}
